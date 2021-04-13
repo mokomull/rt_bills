9a2429b5d7359e1525291cf2b187b4d0fbd9a96c
Matt Mullins <mmullins@mmlx.us>
2021-04-13 14:51:13 -0700

    bill tests: test rolling over the end of the year

--- bill_test.py
+++ bill_test.py
@@ -1,11 +1,11 @@
 import unittest
-import datetime
+from datetime import datetime
 
 from bill import Bill
 
-FIRST = datetime.datetime(2021, 4, 1, 0, 0, 0)
-FIFTEENTH = datetime.datetime(2021, 4, 15, 0, 0, 0)
-TWENTYSEVENTH = datetime.datetime(2021, 4, 27, 0, 0, 0)
+FIRST = datetime(2021, 4, 1, 0, 0, 0)
+FIFTEENTH = datetime(2021, 4, 15, 0, 0, 0)
+TWENTYSEVENTH = datetime(2021, 4, 27, 0, 0, 0)
 
 
 class BilledAndDueInSameMonth(unittest.TestCase):
@@ -18,8 +18,8 @@ class BilledAndDueInSameMonth(unittest.TestCase):
         self.assertEqual(
             self.bill.get_due_date_bounds(FIFTEENTH),
             (
-                datetime.datetime(2021, 3, 25),
-                datetime.datetime(2021, 4, 25),
+                datetime(2021, 3, 25),
+                datetime(2021, 4, 25),
             ),
         )
 
@@ -34,8 +34,8 @@ class DueMonthAfterBilling(unittest.TestCase):
         self.assertEqual(
             self.bill.get_due_date_bounds(FIRST),
             (
-                datetime.datetime(2021, 3, 15),
-                datetime.datetime(2021, 4, 15),
+                datetime(2021, 3, 15),
+                datetime(2021, 4, 15),
             ),
         )
 
@@ -46,8 +46,8 @@ class DueMonthAfterBilling(unittest.TestCase):
         self.assertEqual(
             self.bill.get_due_date_bounds(TWENTYSEVENTH),
             (
-                datetime.datetime(2021, 4, 15),
-                datetime.datetime(2021, 5, 15),
+                datetime(2021, 4, 15),
+                datetime(2021, 5, 15),
             ),
         )
 
@@ -65,8 +65,43 @@ class DueOnFirstOfTheMonth(unittest.TestCase):
         self.assertEqual(
             self.bill.get_due_date_bounds(TWENTYSEVENTH),
             (
-                datetime.datetime(2021, 4, 1),
-                datetime.datetime(2021, 5, 1),
+                datetime(2021, 4, 1),
+                datetime(2021, 5, 1),
+            ),
+        )
+
+
+class AfterDecember(unittest.TestCase):
+    bill = Bill("DueInJanuary", 25, 15)
+
+    def test_dec10(self):
+        self.assertEqual(
+            self.bill.get_due_date_bounds(datetime(2019, 12, 10)),
+            (
+                datetime(2019, 11, 15),
+                datetime(2019, 12, 15),
+            ),
+        )
+
+    def test_dec16(self):
+        self.assertIsNone(
+            self.bill.get_due_date_bounds(datetime(2019, 12, 16), ))
+
+    def test_dec25(self):
+        self.assertEqual(
+            self.bill.get_due_date_bounds(datetime(2019, 12, 25, 0, 0, 1)),
+            (
+                datetime(2019, 12, 15),
+                datetime(2020, 1, 15),
+            ),
+        )
+
+    def test_jan1(self):
+        self.assertEqual(
+            self.bill.get_due_date_bounds(datetime(2020, 1, 1)),
+            (
+                datetime(2019, 12, 15),
+                datetime(2020, 1, 15),
             ),
         )
 
